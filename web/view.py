bbe594cd1a3864d26be99b24283ef1c7bffad3de
Fly-Potato <live1006186250@live.com>
2020-06-19 16:56:14 +0800

    改名+模板

--- web/view.py
+++ web/view.py
@@ -1,16 +1,20 @@
 import nonebot
 from quart_auth import AuthManager, login_required
-from quart import render_template
+from quart import render_template, Blueprint
 import json
+import os
 
 app = nonebot.get_bot().server_app
 auth_manager = AuthManager()
 auth_manager.init_app(app)
 app.secret_key = 'bJmwgxzcR-Ri9ZOWjHVAxw'
+# 修改web模板的位置
+app.template_folder = os.path.join('web', 'templates')
+# 修改静态资源位置
+# app.static_folder = os.path.join('web', 'static')
 
 
 @app.route('/')
-@login_required
 async def index():
     return "index"
 
@@ -18,4 +22,3 @@ async def index():
 @app.route('/login')
 async def login():
     return await render_template('login.html')
-
