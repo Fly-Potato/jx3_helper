e964cdb027c33871602b375658a0cbe6cd06752a
飞翔の小土豆 <dengruiemail@163.com>
2020-06-21 19:46:13 +0800

    update kefu
    删除demo文件夹，dbm功能，增加客服公告查询

---
+++ jx3/plugins/kefu.py
@@ -0,0 +1,9 @@
+from nonebot import on_command, CommandSession
+import httpx
+
+
+@on_command('客服公告')
+async def kefu(session: CommandSession):
+    async with httpx.AsyncClient() as client:
+        res = await client.get("https://jx3.xoyo.com/client/v3_kefu.txt")
+        await session.send(res.text)
